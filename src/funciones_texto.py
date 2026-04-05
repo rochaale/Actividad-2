556a4e778cfdcfa6280bf38a215f084cf91e68f9
Alejandro Rocha <rocha_ale26@hotmail.com>
2026-04-05 20:42:55 -0300

    Ejercicio 1 terminado

--- src/funciones_texto.py
+++ src/funciones_texto.py
@@ -1,6 +1,23 @@
 def contar_lineas(texto):
-    return len(texto.split("\n"))
-
-
     
+    return len(texto.split("."))
+
+def contar_palabras(texto):
+    return len(texto.split())
+
+def palabras_por_linea(texto):
+    total = 0
+    resultado = []
+    for linea in texto.split("."):
+        resultado.append(len(linea.split()))
+    return resultado
+    
+def linea_sobre_promedio(texto):
+    resultado = []
+    promedio = contar_palabras(texto) / contar_lineas(texto)
+    for linea in texto.split("."):
+        if len(linea.split()) > promedio:
+            resultado.append(linea)
+    return resultado
+
     
